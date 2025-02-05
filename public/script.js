293c8aaae8267e0049cab7522039fb0da4ef1a4c
mspace21 <michiotiglao@gmail.com>
2025-02-05 22:28:17 +0800

    updated index.html, style.css, script.js

--- public/script.js
+++ public/script.js
@@ -1,7 +1,21 @@
-const quicknotes = document.getElementById("section1")
-quicknotes.addEventListener("mousemove", (event) => {
-  quicknotes.style.left = event.pageX + 10 + "px";
-  quicknotes.style.top = event.pageY + 10 + "px"; 
-})
+const quicknotes = document.getElementById("section1");
+let isDragging = false;
+let offsetX, offsetY;
 
+quicknotes.addEventListener("mousedown", (event) => {
+  isDragging = true;
+  
+  offsetX = event.clientX - quicknotes.getBoundingClientRect().left;
+  offsetY = event.clientY - quicknotes.getBoundingClientRect().top;
+});
 
+document.addEventListener("mouseup", () => {
+  isDragging = false;
+});
+
+document.addEventListener("mousemove", (event) => {
+  if (isDragging) {
+    quicknotes.style.left = event.clientX - offsetX + "px";
+    quicknotes.style.top = event.clientY - offsetY + "px"; 
+  }
+});

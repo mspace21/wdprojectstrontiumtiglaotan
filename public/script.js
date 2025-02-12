ae3f944670449fc82b0517d378de7df9f51b8e35
mspace21 <michiotiglao@gmail.com>
2025-02-12 20:08:45 +0800

    updated fitness.html and script.js

--- public/script.js
+++ public/script.js
@@ -2,6 +2,8 @@ const userText = document.getElementById("userText");
 const sideBar = document.getElementById("sideBar");
 const homePageContainer = document.getElementById("homePageContainer");
 
+
+
 let userName ='';
 function changeName() {
   userName = prompt("What should we call you?");
@@ -27,4 +29,56 @@ function toggleSidebar() {
     sideBarOpen = false;
     homePageContainer.style.width = "100%"
   } 
+}
+
+const exercises = [
+  { name: "Push-up", target: "Chest, Triceps", bodyweight: true }, 
+  { name: "Pull-up", target: "Back", bodyweight: true }, 
+  { name: "Bench Press", target: "Chest", bodyweight: false },
+  { name: "Overhead Tricep Extension", target: "Triceps", bodyweight: false },
+  { name: "Dumbbell Shoulder Press", target: "Deltoids", bodyweight: false },
+  { name: "Dumbbell Bicep Curl", target: "Biceps", bodyweight: false },
+  { name: "Dumbbell Hammer Curl", target: "Biceps", bodyweight: false },
+  { name: "Dumbbell Concentration Curl", target: "Biceps", bodyweight: false },
+  { name: "Sit-up", target: "Biceps", bodyweight: true },
+  { name: "Bicep Curl", target: "Biceps", bodyweight: false },
+  { name: "Bicep Curl", target: "Biceps", bodyweight: false },
+  { name: "Bicep Curl", target: "Biceps", bodyweight: false },
+]
+
+const exerciseSelect = document.getElementById("exerciseSelect");
+const workoutPlan = document.getElementById("wktdashboardPlan");
+
+exercises.forEach(exercise => {
+  let option = document.createElement("option");
+  option.value = exercise.name;
+  option.textContent = `${exercise.name}`;
+  exerciseSelect.appendChild(option);
+});
+
+function addExercise() {
+  const selectedIndex = exerciseSelect.selectedIndex; // Get the index of selected exercise in the array exercises
+  const selectedExercise = exercises[selectedIndex];
+
+  const exerciseDiv = document.createElement("div");
+  exerciseDiv.className = "exercise";
+  exerciseDiv.innerHTML = `<strong>${selectedExercise.name}</strong>
+                          <button onclick="addSet(this, ${selectedExercise.bodyweight})">Add Set</button>
+                          <div class="sets"></div>`;
+  workoutPlan.appendChild(exerciseDiv);
+}
+
+
+function addSet(button, isBodyweight) {
+  const setsDiv = button.nextElementSibling;
+  const setDiv = document.createElement("div");
+  setDiv.className = "set";
+  setDiv.innerHTML = `Set: <input type="number" placeholder="Reps" min="1">
+                      ${!isBodyweight ? '<input type="number" placeholder="Weight (kg)" min="1">' : ""}
+                      <button onclick="removeSet(this)">Remove</button>`;
+  setsDiv.appendChild(setDiv);
+}
+
+function removeSet(button) {
+    button.parentElement.remove();
 }
